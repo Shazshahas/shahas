d28123e083541d666421f9c2cde22743e6ba1f21
Shazshahas <shahasbashee9544@gmail.com>
2024-12-07 10:42:23 +0530

    hello

---
+++ java.js
@@ -0,0 +1,14 @@
+var a=400
+var b=300
+var c=700
+if(a>b){
+    console.log("a is bigger number",a);
+    
+}
+else{
+    console.log("b is biggest number",b)
+}
+if(ab<c){
+    console.log("c is biggest number",c)
+
+}
